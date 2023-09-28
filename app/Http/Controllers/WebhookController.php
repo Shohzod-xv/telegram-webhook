7d9bb30788f43e764e35c11f00c5b973a28de368
Shohzod-xv <xayrullayevshohzod2@gmail.com>
2023-09-28 19:26:08 +0500

    jasgdcyd

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -6,7 +6,25 @@ use Illuminate\Http\Request;
 
 class WebhookController extends Controller
 {
-    public function index(){
+    public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    {
         return view('webhook');
+    }
+
+    public function webhook(Request $request)
+    {
+        if ($request['method'] == "getUpdates" || $request['method'] == "getMe") {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $request['token'] . "/" . $request['method']);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $results = curl_exec($ch);
+            return view('webhook', compact('results'));
+        }elseif ($request['method'] == "setWebhook") {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $request['token'] . "/" . $request['method'] . "?url=" . $request['address']);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $results = curl_exec($ch);
+            return view('webhook', compact('results'));
+        }
     }
 }
--- resources/views/webhook.blade.php
+++ resources/views/webhook.blade.php
@@ -18,15 +18,26 @@
                                     <input type="text" name="address" class="form-control" placeholder="https://hostname.uz/folder_name/index.php">
                                 </div>
                                 <div class="mt-4 font-weight-light">
-                                    <input type="submit" style="padding: 1px 5px 1px 5px; margin: 4px; color: #000000; background-color: #eea321; border-radius: 4px; border: none" name="create" value="getUpdates"><span>Don't have an account?</span>
+                                    <input type="submit" style="padding: 1px 5px 1px 5px; margin: 4px; color: #000000; background-color: #eea321; border-radius: 4px; border: none" name="method" value="getUpdates"><span>Don't have an account?</span>
                                 </div>
                                 <div class="font-weight-light">
-                                    <input type="submit" style="padding: 1px 5px 1px 5px; margin: 4px; color: #000000; background-color: #1aef1e; border-radius: 4px; border: none" name="create" value="setWebhook"><span>Don't have an account?</span>
+                                    <input type="submit" style="padding: 1px 5px 1px 5px; margin: 4px; color: #000000; background-color: #1aef1e; border-radius: 4px; border: none" name="method" value="setWebhook"><span>Don't have an account?</span>
                                 </div>
                                 <div class="font-weight-light">
-                                    <input type="submit" style="padding: 1px 5px 1px 5px; margin: 4px; color: #ffffff; background-color: #1069e6; border-radius: 4px; border: none" name="create" value="getMe"><span>Don't have an account?</span>
+                                    <input type="submit" style="padding: 1px 5px 1px 5px; margin: 4px; color: #ffffff; background-color: #1069e6; border-radius: 4px; border: none" name="method" value="getMe"><span>Don't have an account?</span>
                                 </div>
                             </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mx-auto mt-5">
+                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                            <span style="color: #000000; font-size: 24px; margin-bottom: 10px">Telegram-бот <b>Ответ вернулся на запрос</b></span>
+                            @php
+                            if(isset($results)){
+                                 $data = json_decode($results, true);
+                             }
+                             @endphp
+                             <pre class="m-3 text-danger"> {{ json_encode($data, JSON_PRETTY_PRINT) }} </pre>
                         </div>
                     </div>
                 </div>

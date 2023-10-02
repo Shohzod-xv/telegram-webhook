eef685fe7ff2e7b7c425dcb5929667a1b482a7ef
Shohzod-xv <xayrullayevshohzod2@gmail.com>
2023-10-02 22:37:19 +0500

    sdhubhuv

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [WebhookController::class, 'index'])->name('home');
-Route::post('/webhook', [WebhookController::class, 'webhook'])->name('webhook');
+Route::post('/main', [WebhookController::class, 'webhook'])->name('webhook');
